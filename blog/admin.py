fa0caa48baf3c07fdb43a441661a789eb3fbb122
Alissonchaves22 <alissonchaves2015@gmail.com>
2021-07-16 19:54:30 -0300

    Terceira mudança

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'autor', 'publicado', 'status']
+    list_filter = ['criado', 'autor', 'publicado', 'status']
+    date_hierarchy = "publicado"
+    raw_id_fields = ("autor", )
+    search_fields = ['titulo', 'conteudo']
+    prepopulated_fields = {'slug': ('titulo',)}
